d7c01bfeb2733e1cac348a18433937031df58163
jeonhaekang <1003gorkd@naver.com>
2023-08-18 20:52:30 +0900

    Feat : useSuspended hooks 추가

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export { useBoolean } from "./useBoolean";
 export { useImmutableState } from "./useImmutableState";
 export { useIntersecting } from "./useIntersecting";
 export { useProgress } from "./useProgress";
+export { useSuspendedQuery } from "./useSuspendedQuery";
