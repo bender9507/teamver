e5a0aa11ebd2039ce8dea4de8416b59320c74eac
wsdx123 <rhftl123@gmail.com>
2023-08-22 14:00:57 +0900

    Feat : mypage 와이어프레임 작업

--- src/pages/mypage/index.page.tsx
+++ src/pages/mypage/index.page.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "next-i18next";
 import Head from "next/head";
 import { Button } from "~/components/Commons";
-import { Flex, Text } from "~/styles/mixins";
+import { Flex, FlexColumn, Text } from "~/styles/mixins";
 import * as Styled from "./mypage.styles";
 
 function MyPage() {
@@ -15,10 +15,46 @@ function MyPage() {
       <Styled.Container>
         <Text>big size Avatar</Text>
         <Button>{t("포지션 수정")}</Button>
-        <Flex>
+        <Styled.LikeUsersButtonContainer>
           <Text>나를 찜한 사용자 n명</Text>
           <Text>화살표버튼</Text>
-        </Flex>
+        </Styled.LikeUsersButtonContainer>
+        <Styled.ProceedingProjectContainer>
+          <Text as="h3" size="heading3">
+            진행중인 프로젝트
+          </Text>
+          <Styled.ProceedingProjectCard>
+            <Text>프로젝트 제목1</Text>
+            <Flex gap={5}>
+              <Button>정보</Button>
+              <Button>멤버</Button>
+            </Flex>
+          </Styled.ProceedingProjectCard>
+        </Styled.ProceedingProjectContainer>
+        <Styled.ReceivedRecommendContainer>
+          <Text as="h3" size="heading3">
+            받은 추천 n
+          </Text>
+          <Styled.RecommendCard>
+            <Flex gap={10}>
+              <Text>middle size Avatar</Text>
+              <FlexColumn>
+                <Text>오너아이디</Text>
+                <Text>프로젝트 참여</Text>
+              </FlexColumn>
+            </Flex>
+            <Text>gdgdgdgdgd</Text>
+          </Styled.RecommendCard>
+        </Styled.ReceivedRecommendContainer>
+        <Styled.PreviousProjectContainer>
+          <Text as="h3" size="heading3">
+            지난 프로젝트
+          </Text>
+          <Styled.PreviousProjectCard>
+            <Text>프로젝트 항목1</Text>
+            <Button>멤버 후기 쓰기</Button>
+          </Styled.PreviousProjectCard>
+        </Styled.PreviousProjectContainer>
       </Styled.Container>
     </>
   );
--- src/pages/mypage/mypage.styles.tsx
+++ src/pages/mypage/mypage.styles.tsx
@@ -1,11 +1,56 @@
 import styled from "@emotion/styled";
-import { flex } from "~/styles/mixins";
+import { flex, size } from "~/styles/mixins";
 
 export const Container = styled.div`
-  ${flex.column({ gap: 15 })};
+  ${flex.column({ gap: 15, align: "center" })};
+  ${size({ width: "100%", maxWidth: 375, minHeight: "100vh" })};
+  margin: 0 auto;
 
   border: 1px solid black;
-  width: 100%;
-  max-width: 375px;
-  min-height: 100vh;
+`;
+
+export const LikeUsersButtonContainer = styled.div`
+  ${flex({ justify: "start" })};
+  ${size({ width: "100%" })};
+  padding: 15px;
+  background-color: #f0f0f0;
+`;
+
+export const ProceedingProjectContainer = styled.div`
+  ${flex.column({ align: "start", gap: 10 })}
+  ${size({ width: "100%" })}
+  padding: 0 15px;
+`;
+
+export const ProceedingProjectCard = styled.div`
+  ${flex({ justify: "between", align: "center" })}
+  ${size({ width: "100%" })}
+  padding: 10px;
+  background-color: #d9d9d9;
+`;
+
+export const ReceivedRecommendContainer = styled.div`
+  ${flex.column({ align: "start", gap: 10 })}
+  ${size({ width: "100%" })}
+  padding: 0 15px;
+`;
+
+export const RecommendCard = styled.div`
+  ${flex.column({ align: "start", gap: 10 })}
+  ${size({ width: "100%" })}
+  padding: 10px;
+  background-color: #d9d9d9;
+`;
+
+export const PreviousProjectContainer = styled.div`
+  ${flex.column({ align: "start", gap: 10 })}
+  ${size({ width: "100%" })}
+  padding: 0 15px;
+`;
+
+export const PreviousProjectCard = styled.div`
+  ${flex({ justify: "between", align: "center" })}
+  ${size({ width: "100%" })}
+  padding: 10px;
+  background-color: #d9d9d9;
 `;
