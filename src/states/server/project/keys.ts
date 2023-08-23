0b4914adeaf5c1bd3e183bb1ea6b3f28b5900740
wsdx123 <rhftl123@gmail.com>
2023-08-24 00:18:24 +0900

    Style : 코드 포매팅

--- src/states/server/project/keys.ts
+++ src/states/server/project/keys.ts
@@ -1,6 +1,6 @@
 const PROJECT_KEY = "PROJECTS";
 
 export const projectsKey = {
-  getProjectsById: (userId: string) => [PROJECT_KEY, 'getProjectsById', userId] as const,
-  getProjects: () => [PROJECT_KEY, 'getProjects'] as const
-} as const
+  getProjectsById: (userId: string) => [PROJECT_KEY, "getProjectsById", userId] as const,
+  getProjects: () => [PROJECT_KEY, "getProjects"] as const
+} as const;
--- src/states/server/review/queries.ts
+++ src/states/server/review/queries.ts
@@ -1,11 +1,11 @@
-import { useQuery } from "@tanstack/react-query"
-import { getReviews } from "./apis"
-import { reviewsKey } from "./keys"
+import { useQuery } from "@tanstack/react-query";
+import { getReviews } from "./apis";
+import { reviewsKey } from "./keys";
 
 export const useGetReviewQuery = (userId: string) => {
   return useQuery({
     queryKey: reviewsKey.getReviewsById(userId),
     queryFn: () => getReviews(userId),
     initialData: []
-  })
-}
+  });
+};
