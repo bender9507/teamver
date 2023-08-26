2d0e7a0b1b7e0286461aa1bb10cb3ceb698b4357
jeonhaekang <1003gorkd@naver.com>
2023-08-26 18:21:14 +0900

    Refactor : Profiles 테이블 수정으로 인한 업데이트

--- src/states/server/profile/constants.ts
+++ src/states/server/profile/constants.ts
@@ -1,9 +1,10 @@
 export const PROFILE_ALL_DATA_QUERY = `
     *,
+    job:constantJobs(*),
+    role:constantRoles(*),
     languages:profileLanguages!inner(...constantLanguages(*)),
     skills:profileSkills!inner(...constantSkills(*)),
     areas:profileAreas!inner(...constantAreas(*)),
-    jobs:profileJobs!inner(...constantJobs(*)),
     projectTypes:profileProjectTypes!inner(...constantProjectTypes(*)),
     personalities:profilePersonalities!inner(...constantPersonalities(*)),
     positions:profilePositions!inner(...constantPositions(*))
