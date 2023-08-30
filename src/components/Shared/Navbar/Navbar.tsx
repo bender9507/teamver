19266fa7c78030c8697926164129e2d03b8c606f
jeonhaekang <1003gorkd@naver.com>
2023-08-31 00:34:03 +0900

    Refactor : Navbar 아이콘 적용

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,41 +1,30 @@
 import { useUser } from "@supabase/auth-helpers-react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { Icon } from "~/components/Commons";
 import { routes } from "~/constants/routes";
-import { FlexCenter, Text } from "~/styles/mixins";
 import * as Styled from "./Navbar.styles";
 
 export const Navbar = () => {
+  const { pathname } = useRouter();
   const user = useUser();
 
   return (
     <Styled.Navbar>
       <Link href={routes.main}>
-        <FlexCenter gap={4} direction="column">
-          <Icon name="home" />
-          <Text>Home</Text>
-        </FlexCenter>
+        <Icon name={pathname.startsWith(routes.main) ? "homeFill" : "home"} />
       </Link>
 
       <Link href={routes.chat}>
-        <FlexCenter gap={4} direction="column">
-          <Icon name="chat" />
-          <Text>Chat</Text>
-        </FlexCenter>
+        <Icon name={pathname.startsWith(routes.chat) ? "chatFill" : "chat"} />
       </Link>
 
       <Link href={routes.like}>
-        <FlexCenter gap={4} direction="column">
-          <Icon name="list" />
-          <Text>List</Text>
-        </FlexCenter>
+        <Icon name={pathname.startsWith(routes.like) ? "likeFill" : "like"} />
       </Link>
 
       <Link href={routes.profile(user?.id ?? "")}>
-        <FlexCenter gap={4} direction="column">
-          <Icon name="my" />
-          <Text>My</Text>
-        </FlexCenter>
+        <Icon name={pathname.startsWith("/profile") ? "profileFill" : "profile"} />
       </Link>
     </Styled.Navbar>
   );
