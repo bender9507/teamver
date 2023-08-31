416cfd4129f4ce96dfbc3e59460c8387eed96dfc
jeonhaekang <1003gorkd@naver.com>
2023-08-31 17:01:52 +0900

    Feat : PWA 적용

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
