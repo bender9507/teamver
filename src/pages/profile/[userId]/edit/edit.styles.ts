dcb62bc4c66d7d4a91ae37eb25c61951bc63410a
wsdx123 <rhftl123@gmail.com>
2023-09-01 14:40:36 +0900

    fix : 프로필 수정 페이지 헤더 추가, 프사 변경 버튼 영역 줄이기

--- src/pages/profile/[userId]/edit/edit.styles.ts
+++ src/pages/profile/[userId]/edit/edit.styles.ts
@@ -1,8 +1,18 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
-import { flex } from "~/styles/mixins";
+import { flex, grid, size } from "~/styles/mixins";
 
 export const Container = styled.form`
   ${flex.column({ gap: 46 })};
 
   padding: 32px;
+`;
+
+export const Header = styled.header`
+  ${grid({ column: 3, justify: "start", align: "center" })};
+  ${size({ width: "100%" })}
+
+  ${({ theme: { sizes } }) => css`
+    height: ${sizes.height.header}px;
+  `}
 `;
