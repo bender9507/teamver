059ea7929f8dd7576e3a9a638ceeeaaca4000765
jeonhaekang <1003gorkd@naver.com>
2023-09-02 22:56:12 +0900

    Design : 찜 쪽 디자인 적용

--- src/components/Like/Like.styles.ts
+++ src/components/Like/Like.styles.ts
@@ -1,30 +1,5 @@
-import { css } from "@emotion/react";
 import styled from "@emotion/styled";
-import { FlexColumn, flex, grid, size } from "~/styles/mixins";
-
-export const Container = styled(FlexColumn)`
-  ${({
-    theme: {
-      sizes: { height }
-    }
-  }) => {
-    const _height = `calc(100vh - ${height.navbar}px)`;
-
-    return css`
-      ${size({ width: "100%", height: _height })}
-    `;
-  }}
-  padding: 20px;
-`;
-
-export const Header = styled.header`
-  ${grid({ column: 3, justify: "start", align: "center" })};
-  ${size({ width: "100%" })}
-
-  ${({ theme: { sizes } }) => css`
-    height: ${sizes.height.header}px;
-  `}
-`;
+import { flex } from "~/styles/mixins";
 
 export const Card = styled.div`
   ${flex({ align: "center", justify: "between" })}
--- src/components/Like/Member/Member.tsx
+++ src/components/Like/Member/Member.tsx
@@ -1,34 +1,24 @@
 import { useTranslation } from "next-i18next";
-import { IconButton } from "~/components/Commons";
 import { FlexColumn, Text } from "~/styles/mixins";
-import * as Styled from "../Like.styles";
 
+import { useSelectFollowProjectsQuery } from "~/states/server/project";
 import { LikeCardMember } from "../LikeCardMember";
-import { useMember } from "./Member.hooks";
 
 export const Member = ({ userId }: { userId: string }) => {
-  const app = useMember(userId);
+  const { data: followProjects } = useSelectFollowProjectsQuery(userId);
   const { t } = useTranslation("like");
 
   return (
-    <Styled.Container>
-      <Styled.Header>
-        <IconButton type="button" name="arrowBack" color="content1" onClick={app.handleBack} />
-
-        <Text style={{ margin: "0 auto" }} as="h3" size="heading3">
-          {t("찜 목록")}
-        </Text>
-      </Styled.Header>
-
-      <Text as="h4" size="heading4" style={{ margin: "40px 0 20px 0" }}>
+    <>
+      <Text as="h4" size="textLarge">
         {t("내가 찜한 프로젝트")}
       </Text>
 
-      <FlexColumn gap={15}>
-        {app.followProjects.map((followProject) => (
+      <FlexColumn gap={15} marginTop={16}>
+        {followProjects.map((followProject) => (
           <LikeCardMember key={followProject.id} data={followProject} userId={userId} />
         ))}
       </FlexColumn>
-    </Styled.Container>
+    </>
   );
 };
