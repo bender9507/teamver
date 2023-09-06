77e30c07facf8c0ea3d6a9c4fc94291712cacf4e
jeonhaekang <1003gorkd@naver.com>
2023-09-06 19:23:31 +0900

    Refactor : 채팅 메세지 영역 레이아웃 재설정

--- src/pages/chat/room/components/ChatMessageBox/ChatMessageBox.tsx
+++ src/pages/chat/room/components/ChatMessageBox/ChatMessageBox.tsx
@@ -2,10 +2,12 @@ import dayjs from "dayjs";
 import { useTranslation } from "next-i18next";
 import { Avatar, IconButton } from "~/components/Commons";
 import type { ProfileAllDataRow } from "~/states/server/profile";
-import { FlexColumn, LayoutContent, PosCenter, Position, SizeBox, Text } from "~/styles/mixins";
+import { PosCenter, Position, SizeBox, Text } from "~/styles/mixins";
 import { isEmpty } from "~/utils";
 import { useChatMessageBox } from "./ChatMessageBox.hooks";
 import { Message } from "./Messages";
+
+import * as Styled from "./ChatMessageBox.styles";
 
 export const ChatMessageBox = ({ opponent }: { opponent: ProfileAllDataRow }) => {
   const app = useChatMessageBox();
@@ -14,49 +16,47 @@ export const ChatMessageBox = ({ opponent }: { opponent: ProfileAllDataRow }) =>
   let lastTimestamp: Date | null = null;
 
   return (
-    <>
-      <LayoutContent onScroll={app.handleScroll}>
-        {isEmpty(app.messages) && (
-          <PosCenter>
-            <Text as="p" textAlign="center" size="textMediumBold" color="gray6">
-              {t("NAME님과 팀원 매칭 되었어요", { name: opponent.name })}
-            </Text>
+    <Styled.Container>
+      {isEmpty(app.messages) && (
+        <PosCenter>
+          <Text as="p" textAlign="center" size="textMediumBold" color="gray6">
+            {t("NAME님과 팀원 매칭 되었어요", { name: opponent.name })}
+          </Text>
 
-            <SizeBox height={24} />
+          <SizeBox height={24} />
 
-            <Avatar size="xLarge" src={opponent.imageUrl} />
-          </PosCenter>
-        )}
+          <Avatar size="xLarge" src={opponent.imageUrl} />
+        </PosCenter>
+      )}
 
-        <FlexColumn gap={10} padding="26px 32px 7px 32px">
-          {app.messages.map((message, index) => {
-            const isChaining = dayjs(lastTimestamp).minute() === dayjs(message.createdAt).minute();
+      <Styled.ChatBox onScroll={app.handleScroll}>
+        {app.messages.map((message, index) => {
+          const isChaining = dayjs(lastTimestamp).minute() === dayjs(message.createdAt).minute();
 
-            const isChainingEnd =
-              dayjs(message.createdAt).minute() !==
-              dayjs(app.messages[index + 1]?.createdAt).minute();
+          const isChainingEnd =
+            dayjs(message.createdAt).minute() !==
+            dayjs(app.messages[index + 1]?.createdAt).minute();
 
-            lastTimestamp = message.createdAt;
+          lastTimestamp = message.createdAt;
 
-            return (
-              <Message
-                key={message.id}
-                isChaining={isChaining}
-                isChainingEnd={isChainingEnd}
-                message={message}
-              />
-            );
-          })}
-        </FlexColumn>
+          return (
+            <Message
+              key={message.id}
+              isChaining={isChaining}
+              isChainingEnd={isChainingEnd}
+              message={message}
+            />
+          );
+        })}
 
         <div ref={app.bottomRef} />
-      </LayoutContent>
+      </Styled.ChatBox>
 
       {!app.isScrollEnd && (
-        <Position position="absolute" bottom={80} right={20}>
+        <Position position="absolute" bottom={0} right={20}>
           <IconButton name="downButton" color="content1" onClick={() => app.handleScrollToEnd()} />
         </Position>
       )}
-    </>
+    </Styled.Container>
   );
 };
