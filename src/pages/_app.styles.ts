fc3cedf9e059f99641d4943aa92bb7ea194ffd58
jeonhaekang <1003gorkd@naver.com>
2023-09-07 20:23:52 +0900

    Refactor : 100svh => 100vh 로 단위 변경

--- src/pages/_app.styles.ts
+++ src/pages/_app.styles.ts
@@ -5,7 +5,12 @@ export const Container = styled.div`
   position: relative;
   overflow: hidden;
 
-  ${size({ width: "100%", maxWidth: "500px", minHeight: "100svh", maxHeight: "100svh" })};
+  ${size({ width: "100%", maxWidth: "500px", minHeight: "100vh", maxHeight: "100vh" })};
+
+  @supports (-webkit-touch-callout: none) {
+    min-height: -webkit-fill-available;
+    max-height: -webkit-fill-available;
+  }
 
   margin: 0 auto;
 `;
