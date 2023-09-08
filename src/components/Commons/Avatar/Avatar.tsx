dc6e844797f4a9222ebdf591accd3597ed76dfc1
subin <fnqhdtm@gmail.com>
2023-09-08 10:24:02 +0900

    Refactor : Avatar props 추가

--- src/components/Commons/Avatar/Avatar.tsx
+++ src/components/Commons/Avatar/Avatar.tsx
@@ -1,10 +1,10 @@
 import * as Styled from "./Avatar.styles";
 import type { AvatarProps } from "./Avatar.types";
 
-export const Avatar = ({ size, src, shape }: AvatarProps) => {
+export const Avatar = ({ size, src, shape, ...props }: AvatarProps) => {
   return (
     <Styled.AvatarContainer size={size} shape={shape}>
-      <Styled.Avatar fill src={src} sizes="100%" alt="프로필 이미지" priority />
+      <Styled.Avatar fill src={src} sizes="100%" alt="프로필 이미지" {...props} />
     </Styled.AvatarContainer>
   );
 };
