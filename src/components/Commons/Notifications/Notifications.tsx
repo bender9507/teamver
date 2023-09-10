bdf771c89940f6f31e984d36f983584133f253ec
wsdx123 <rhftl123@gmail.com>
2023-09-11 01:08:39 +0900

    Feat : 앱을 켜놓은 상태가 아니어도 알림이 발생하게 작성(그전 로직은 접속했을때만 기준으로 작성되었음)

--- src/components/Commons/Notifications/Notifications.tsx
+++ src/components/Commons/Notifications/Notifications.tsx
@@ -1,32 +1,15 @@
 import { useUser } from "@supabase/auth-helpers-react";
-import type {
-  RealtimePostgresInsertPayload,
-  RealtimePostgresUpdatePayload,
-  User
-} from "@supabase/supabase-js";
+import type { RealtimePostgresUpdatePayload, User } from "@supabase/supabase-js";
 import { useQueryClient } from "@tanstack/react-query";
 import { useMount } from "react-use";
 import type { ChatRequestMemberRow, ChatRequestOwnerRow } from "~/states/server/chat";
 import { supabase } from "~/states/server/config";
 import { noticeKeys } from "~/states/server/notice/keys";
-import { useInsertNoticeMember, useInsertNoticeOwner } from "~/states/server/notice/mutations";
 import type { ProjectInviteRow } from "~/states/server/project";
 
 export const Notifications = () => {
   const user = useUser() as User;
   const queryClient = useQueryClient();
-
-  const { mutate: insertNoticeMemberMutate } = useInsertNoticeMember({
-    onSuccess: () => {
-      queryClient.invalidateQueries(noticeKeys.selectNoticeMember(user.id));
-    }
-  });
-
-  const { mutate: insertNoticeOwnerMutate } = useInsertNoticeOwner({
-    onSuccess: () => {
-      queryClient.invalidateQueries(noticeKeys.selectNoticeOwner(user.id));
-    }
-  });
 
   useMount(() => {
     if (!user.id) return;
@@ -41,14 +24,8 @@ export const Notifications = () => {
           table: "chatRequestMember",
           filter: `receiverId=eq.${user.id}`
         },
-        (payload: RealtimePostgresInsertPayload<ChatRequestMemberRow>) => {
-          console.log("im owner"); // 내가 오너일때 멤버모드인 다른사람이 나에게 채팅요청했을때 알림
-          console.log(payload);
-          insertNoticeOwnerMutate({
-            receiverId: user.id,
-            requesterId: payload.new.requesterId,
-            state: "ChatRequest"
-          });
+        () => {
+          queryClient.invalidateQueries(noticeKeys.selectNoticeOwner(user.id));
         }
       )
       .on(
@@ -61,14 +38,7 @@ export const Notifications = () => {
         },
         (payload: RealtimePostgresUpdatePayload<ChatRequestMemberRow>) => {
           if (payload.new.state === "GRANT") {
-            console.log("im member"); // 내가 멤버일때 내가보낸 채팅요청을 오너가 수락했을때 알림
-            console.log(payload);
-
-            insertNoticeMemberMutate({
-              receiverId: user.id,
-              requesterId: payload.new.receiverId,
-              state: "ChatGranted"
-            });
+            queryClient.invalidateQueries(noticeKeys.selectNoticeMember(user.id));
           }
         }
       )
@@ -81,14 +51,9 @@ export const Notifications = () => {
           filter: `requesterId=eq.${user.id}`
         },
         (payload: RealtimePostgresUpdatePayload<ChatRequestOwnerRow>) => {
-          console.log("im owner"); // 내가 오너일때 내가보낸 채팅요청을 멤버가 수락했을때 알림
-          console.log(payload);
-
-          insertNoticeOwnerMutate({
-            receiverId: user.id,
-            requesterId: payload.new.receiverId,
-            state: "ChatGranted"
-          });
+          if (payload.new.state === "GRANT") {
+            queryClient.invalidateQueries(noticeKeys.selectNoticeOwner(user.id));
+          }
         }
       )
       .on(
@@ -99,15 +64,8 @@ export const Notifications = () => {
           table: "chatRequestOwner",
           filter: `receiverId=eq.${user.id}`
         },
-        (payload: RealtimePostgresInsertPayload<ChatRequestOwnerRow>) => {
-          console.log("im member"); // 내가 멤버일때 오너모드인 다른사람이 나를 채팅요청 했을때 알림
-          console.log(payload);
-
-          insertNoticeMemberMutate({
-            receiverId: user.id,
-            requesterId: payload.new.requesterId,
-            state: "ChatRequest"
-          });
+        () => {
+          queryClient.invalidateQueries(noticeKeys.selectNoticeMember(user.id));
         }
       )
       .on(
@@ -118,15 +76,8 @@ export const Notifications = () => {
           table: "projectInvite",
           filter: `receiverId=eq.${user.id}`
         },
-        (payload: RealtimePostgresInsertPayload<ProjectInviteRow>) => {
-          console.log("im member"); // 내가 멤버일때 오너모드인 다른사람이 나를 프로젝트에 초대했을때 알림
-          console.log(payload);
-
-          insertNoticeMemberMutate({
-            receiverId: user.id,
-            requesterId: payload.new.requesterId,
-            state: "TeamRequest"
-          });
+        () => {
+          queryClient.invalidateQueries(noticeKeys.selectNoticeMember(user.id));
         }
       )
       .on(
@@ -138,14 +89,9 @@ export const Notifications = () => {
           filter: `requesterId=eq.${user.id}`
         },
         (payload: RealtimePostgresUpdatePayload<ProjectInviteRow>) => {
-          console.log("im owner"); // 내가 오너일때 내가보낸 프로젝트 초대를 멤버가 수락했을때 알림
-          console.log(payload);
-
-          insertNoticeOwnerMutate({
-            receiverId: user.id,
-            requesterId: payload.new.receiverId,
-            state: "TeamGranted"
-          });
+          if (payload.new.state === "GRANT") {
+            queryClient.invalidateQueries(noticeKeys.selectNoticeOwner(user.id));
+          }
         }
       )
       .subscribe();
