d9836718e23d8f1ab10bb515373ca4328db254e5
HyunseoY <maricat0715@gmail.com>
2023-09-11 19:39:58 +0900

    Fix: 줄바꿈 없이 소개 500자 입력시 레이아웃 깨지는 문제 해결

--- src/pages/boarding/index.page.tsx
+++ src/pages/boarding/index.page.tsx
@@ -1,12 +1,7 @@
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
-import type { User } from "@supabase/supabase-js";
-import type { GetServerSideProps } from "next";
 import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Link from "next/link";
 import { Button } from "~/components/Commons";
 import { routes } from "~/constants/routes";
-import { selectProfile } from "~/states/server/profile";
 import * as Styled from "./boarding.styles";
 import BoardCarousel from "./components/Carousel/Carousel";
 
@@ -27,29 +22,3 @@ const Boarding = () => {
 };
 
 export default Boarding;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const supabaseServer = createPagesServerClient(context);
-
-  const { data: userData } = await supabaseServer.auth.getUser();
-
-  const user = userData.user as User;
-
-  const profile = await selectProfile(user.id);
-
-  if (profile) {
-    return {
-      redirect: {
-        destination: routes.home,
-        permanent: false
-      }
-    };
-  }
-
-  return {
-    props: {
-      user,
-      ...(await serverSideTranslations(context.locale as string, ["boarding"]))
-    }
-  };
-};
