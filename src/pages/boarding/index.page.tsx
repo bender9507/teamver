896a8a5cf2794faaf35634a0404838e86ecc3120
subin <fnqhdtm@gmail.com>
2023-09-11 20:06:08 +0900

    Refactor : 온보딩 페이지 다국어 서버사이드 추가

--- src/pages/boarding/index.page.tsx
+++ src/pages/boarding/index.page.tsx
@@ -1,12 +1,10 @@
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
-import type { User } from "@supabase/supabase-js";
 import type { GetServerSideProps } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Link from "next/link";
 import { Button } from "~/components/Commons";
 import { routes } from "~/constants/routes";
-import { selectProfile } from "~/states/server/profile";
+import { requireAuthentication } from "~/utils";
 import * as Styled from "./boarding.styles";
 import BoardCarousel from "./components/Carousel/Carousel";
 
@@ -28,28 +26,13 @@ const Boarding = () => {
 
 export default Boarding;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const supabaseServer = createPagesServerClient(context);
-
-  const { data: userData } = await supabaseServer.auth.getUser();
-
-  const user = userData.user as User;
-
-  const profile = await selectProfile(user.id);
-
-  if (profile) {
+export const getServerSideProps: GetServerSideProps = requireAuthentication(
+  async (context, session) => {
     return {
-      redirect: {
-        destination: routes.home,
-        permanent: false
+      props: {
+        session,
+        ...(await serverSideTranslations(context.locale as string, ["boarding"]))
       }
     };
   }
-
-  return {
-    props: {
-      user,
-      ...(await serverSideTranslations(context.locale as string, ["boarding"]))
-    }
-  };
-};
+);
