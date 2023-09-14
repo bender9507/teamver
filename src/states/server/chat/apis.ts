669d8ad2e5392e0475ca394b65617093e4c5bda7
wsdx123 <rhftl123@gmail.com>
2023-09-14 22:40:06 +0900

    Refactor : 사용하지않는 api 제거

--- src/states/server/chat/apis.ts
+++ src/states/server/chat/apis.ts
@@ -7,10 +7,8 @@ import type {
   ChatMessageInsert,
   ChatRequestMemberAllData,
   ChatRequestMemberInsert,
-  ChatRequestMemberRow,
   ChatRequestOwnerAllData,
   ChatRequestOwnerInsert,
-  ChatRequestOwnerRow,
   ChatRoomAllData
 } from "./types";
 
@@ -24,56 +22,6 @@ export const insertChatRequestMember = async (chatRequest: ChatRequestMemberInse
   const { error } = await supabase.from("chatRequestMember").insert(chatRequest);
 
   if (error) throw Error("채팅 요청에 실패하였습니다.");
-};
-
-export const selectChatRequestsMember = async ({
-  requesterId,
-  state
-}: {
-  requesterId: string;
-  state: "PENDING" | "GRANT" | "DENIED" | "ALL";
-}) => {
-  let query = supabase
-    .from("chatRequestMember")
-    .select(`*, receiverProfile:receiverId(${PROFILE_ALL_DATA_QUERY})`)
-    .eq("requesterId", requesterId);
-
-  if (state !== "ALL") {
-    query = query.eq("state", state);
-  }
-
-  const { data, error } = await query.returns<
-    (Pick<ChatRequestMemberRow, "id" | "state"> & { receiverProfile: ProfileAllDataRow })[]
-  >();
-
-  if (error) throw Error("채팅 요청 목록을 불러오는데 실패하였습니다.");
-
-  return data;
-};
-
-export const selectChatRequestsOwner = async ({
-  requesterId,
-  state
-}: {
-  requesterId: string;
-  state: "PENDING" | "GRANT" | "DENIED" | "ALL";
-}) => {
-  let query = supabase
-    .from("chatRequestOwner")
-    .select(`*, receiverProfile:receiverId(${PROFILE_ALL_DATA_QUERY})`)
-    .eq("requesterId", requesterId);
-
-  if (state !== "ALL") {
-    query = query.eq("state", state);
-  }
-
-  const { data, error } = await query.returns<
-    (Pick<ChatRequestOwnerRow, "id" | "state"> & { receiverProfile: ProfileAllDataRow })[]
-  >();
-
-  if (error) throw Error("채팅 요청 목록을 불러오는데 실패하였습니다.");
-
-  return data;
 };
 
 export const deleteChatRequestMember = async (id: number) => {
